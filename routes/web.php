e438ac5407275bc134a3ae992541829a759c56f3
Christian Gonçalves <chrisapppg1@gmail.com>
2026-03-18 18:07:09 -0300

    Atualiza rotas de produção

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,7 @@
 use App\Http\Controllers\VirtualMeetingController;
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/', '/reunioes-virtuais');
 
-Route::get('/reunioes-virtuais', [VirtualMeetingController::class, 'index'])
+Route::get('/', [VirtualMeetingController::class, 'index'])
     ->middleware('throttle:web-public')
     ->name('virtual-meetings.index');
